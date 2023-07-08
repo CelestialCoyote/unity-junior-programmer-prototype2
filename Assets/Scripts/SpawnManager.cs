a9dba9d6d03417af738cb048bb41ba3b76833e0a
Paul Stearns <celestialcoyote@gmail.com>
2023-07-08 16:36:51 -0700

    added collision detection

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,27 +6,31 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
 
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
 
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+    }
 
-            Instantiate(
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(
                 animalPrefabs[animalIndex],
                 spawnPos,
                 animalPrefabs[animalIndex].transform.rotation
-            );
-        }
+        );
     }
 }
