59ff4245d70112056efc5e112c18ca31b76de891
Paul Stearns <celestialcoyote@gmail.com>
2023-07-10 18:07:21 -0700

    added random ball drop interval

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,16 +11,17 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float randomSpawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
+        
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         int ballIndex = Random.Range(0, ballPrefabs.Length);
 
@@ -28,12 +29,14 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        //Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
         Instantiate(
                 ballPrefabs[ballIndex],
                 spawnPos,
                 ballPrefabs[ballIndex].transform.rotation
         );
-    }
 
+        randomSpawnInterval = Random.Range(2.0f, 5.0f);
+        Invoke("SpawnRandomBall", randomSpawnInterval);
+        //Debug.Log(randomSpawnInterval);
+    }
 }
